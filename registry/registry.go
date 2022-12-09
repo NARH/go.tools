0a25b6539d1fffbf80d51496298a42762606d8ff
Hideki Narita <naritaenator@gmail.com>
2022-12-10 00:57:19 +0900

    Changes to be committed: 	modified:   registry/registry.go

--- registry/registry.go
+++ registry/registry.go
@@ -33,6 +33,7 @@ type hive struct {
 
 // レジストリデータ構造体
 type registry struct {
+	data map[string]interface{}
 }
 
 // Registry Store レジストリの格納場所
@@ -69,6 +70,7 @@ const HIVE_NOT_END_WITH = `/`
 
 // hive を作成する
 func hiveCreate(uri string) *hive {
+	// 長さ 0 は禁止は禁止
 	if 0 >= len(uri) {
 		panic(fmt.Errorf("hive name is empty."))
 	}
@@ -80,7 +82,58 @@ func hiveCreate(uri string) *hive {
 		return &hive{uri: uri}
 	}
 
+	// NGの場合はpanicでerrorを投げる
 	panic(fmt.Errorf("Illegal hive name [%s]", uri))
+}
+
+// 新規レジストリデータを作成する
+func NewRegistry() *registry {
+	return &registry{data: map[string]interface{}{}}
+}
+
+// レジストリデータ最大サイズ長(以下)
+const MAX_KEY_LENGTH = 256
+
+// keyのチェック
+func (r *registry) keyCheck(key string) {
+	// Keyサイズチェック
+	if 1 > len(key) && MAX_KEY_LENGTH < len(key) {
+		panic(fmt.Errorf("Illegal key length. [%s:%d]", key, len(key)))
+	}
+}
+
+// レジストリデータにKey=Valueを追加する
+func (r *registry) Append(key string, val *interface{}) {
+	// keyのチェック
+	r.keyCheck(key)
+
+	// Appendなので登録済みのKeyはエラー
+	if _, ok := r.data[key]; ok {
+		panic(fmt.Errorf("key [%s] is already registed.", key))
+	}
+
+	r.data[key] = val
+}
+
+// レジストリデータからKeyに対応したValueを取得する
+func (r *registry) Get(key string) (*interface{}, error) {
+	// keyのチェック時にエラーとして返す
+	defer func() (*interface{}, error) {
+		e := recover()
+		if nil != e {
+			if ee, ok := e.(error); ok {
+				return nil, ee
+			}
+		}
+		panic(fmt.Errorf("UnExpcted key [%s]", key))
+	}()
+
+	r.keyCheck(key)
+
+	if val, ok := r.data[key]; ok {
+		return &val, nil
+	}
+	return nil, fmt.Errorf("key [%s] is not registed.", key)
 }
 
 // レジストリパッケージ関数 Add()
